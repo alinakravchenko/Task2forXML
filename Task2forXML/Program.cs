676050eead992ea2be9b4306292f95ef76902fa2
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-10-10 20:27:03 +0300

    XML

--- Task2forXML/Program.cs
+++ Task2forXML/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using System.Xml;
 
 
@@ -13,40 +14,17 @@ namespace XMLWriterReader
                 new Product("Asus", "Офисный унылый", 30000),
                 new Product("Apple", "Престижный донельзя", 200000)
             };
+            List<Product> listProduct2 = new List<Product>();
+            Order ord2 = new Order(listProduct2);
             Order ord = new Order(listProduct);
-            XmlTextWriter? xmltw = null;
-            try
+            MyXML.SaveXML("test.xml", ord);
+            MyXML.XMLLoad("test.xml");
+            ord2 = MyXML.XMLLoad("test.xml", ord2);
+            foreach (Product product in ord2._products)
             {
-                xmltw = new XmlTextWriter("test.xml", Encoding.Unicode);
-                xmltw.Formatting = Formatting.Indented;
-                xmltw.Indentation = 4;
-                xmltw.WriteStartDocument();
-                xmltw.WriteStartElement("orders");
-                foreach (Product product in ord._products)
-                {
-                    xmltw.WriteStartElement("Product");
-                    xmltw.WriteAttributeString("name", product.Name);
-                    xmltw.WriteElementString("description", product.Description);
-                    xmltw.WriteElementString("price", product.Price.ToString());
-                    xmltw.WriteEndElement();
-                }
-                xmltw.WriteEndElement();
-                xmltw.WriteEndDocument();
+                Console.WriteLine(product.ToString());
+            }
 
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                if (xmltw != null)
-                    xmltw.Close();
-            }
         }
     }
 
@@ -73,6 +51,128 @@ namespace XMLWriterReader
             Name = name;
             Description = desc;
             Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name } + {Description} + {Price}";
+        }
+
+    }
+
+    class MyXML
+    {
+        public static void SaveXML(string path, Order ord)
+        {
+            XmlTextWriter? xmltw = null;
+            try
+            {
+                xmltw = new XmlTextWriter(path, Encoding.Unicode);
+                xmltw.Formatting = Formatting.Indented;
+                xmltw.Indentation = 2;
+                xmltw.WriteStartDocument();
+                xmltw.WriteStartElement("orders");
+                foreach (Product product in ord._products)
+                {
+                    xmltw.WriteStartElement("Product");
+                    xmltw.WriteAttributeString("name", product.Name);
+                    xmltw.WriteElementString("description", product.Description);
+                    xmltw.WriteElementString("price", product.Price.ToString());
+                    xmltw.WriteEndElement();
+                }
+                xmltw.WriteEndElement();
+                xmltw.WriteEndDocument();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (xmltw != null)
+                    xmltw.Close();
+            }
+        }
+
+        public static void XMLLoad(string path)
+        {
+
+            XmlTextReader? xmltr = null;
+            List<Product> products = new List<Product>();
+            Product prod = new Product("", "", 0);
+            try
+            {
+                xmltr = new XmlTextReader(path);
+                string name = "", description = "";
+                int price = 0;
+                while (xmltr.Read())
+                {
+                    if (xmltr.NodeType == XmlNodeType.Element && xmltr.Name == "Product")
+                    {
+                        prod = new Product("", "", 0);
+                        xmltr.MoveToAttribute(0);
+                        name = xmltr.Value;
+                    }
+                    if (xmltr.NodeType == XmlNodeType.Element && xmltr.Name == "description")
+                    {
+                        xmltr.MoveToContent();
+                        description = xmltr.ReadString();
+                    }
+                    if (xmltr.NodeType == XmlNodeType.Element && xmltr.Name == "price")
+                    {
+                        xmltr.MoveToContent();
+                        price = Int32.Parse(xmltr.ReadString());
+                    }
+                    if (xmltr.NodeType == XmlNodeType.EndElement && xmltr.Name == "Product")
+                    {
+                        prod.Name = name;
+                        prod.Description = description;
+                        prod.Price = price;
+                        if (prod.Name.Length > 1 && prod.Description.Length > 1 && prod.Price > 0)
+                        {
+                            products.Add(prod);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error read xml-file");
+                            Console.WriteLine(prod.ToString());
+                        }
+                    }
+                }
+                foreach (Product product in products)
+                {
+                    Console.WriteLine(product.ToString());
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+            finally
+            {
+                if (xmltr != null)
+                    xmltr.Close();
+            }
+        }
+
+        public static Order XMLLoad(string path, Order ord)
+        {
+            XmlDocument xdoc = new XmlDocument(); ;
+            if (path != null) xdoc.Load(path);
+            foreach (XmlElement xel in xdoc.GetElementsByTagName("Product"))
+            {
+                ord._products.Add(new Product(xel.GetAttribute("name"), xel.GetElementsByTagName("description")[0].InnerText,Int32.Parse(xel.GetElementsByTagName("price")[0].InnerText)));
+                
+            }
+            return ord;
         }
 
 
